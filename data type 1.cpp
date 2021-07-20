6a70435584484e4bfc81823ff58db041e1b3336c
20wh1a1298 <85934726+20wh1a1298@users.noreply.github.com>
2021-07-20 15:23:24 +0530

    Add files via upload

---
+++ data type 1.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	char x,y,z;
+	     printf("enter three chars\n");
+	     scanf("%c%c%c",&x,&y,&z);
+	     printf("after reversing\n");
+	     printf("%c,%c,%c",z,y,x);
+	     return 0;
+	     
+}
