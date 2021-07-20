524925aab1894dbc9d3ecce6d5559b42dd3a6096
20wh1a1298 <85934726+20wh1a1298@users.noreply.github.com>
2021-07-20 15:55:24 +0530

    Add files via upload

---
+++ sum , product and average of 3 integers.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int n1,n2,n3;
+	printf("enter 3 numbers\n");
+	scanf("%d%d%d",&n1,&n2,&n3);
+	printf("sum=%d\n",n1+n2+n3);
+	printf("product=%d\n",n1*n2*n3);
+	printf("average=%f\n",(n1+n2+n3)/3.0);
+	return 0;
+	
+}
