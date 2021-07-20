cbc632b35d7d34a7e2dfdf25097713ff1545a471
20wh1a1298 <85934726+20wh1a1298@users.noreply.github.com>
2021-07-20 15:37:13 +0530

    Add files via upload

---
+++ simple interest.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int p,t,r;
+	float SI;
+	printf("enter p,t,r values \n");
+	scanf("%d%d%d",&p,&t,&r);
+	SI=(p*t*r)/100.0;
+	printf("simplr interest=%f",SI);
+	return 0;
+	
+	
+}
