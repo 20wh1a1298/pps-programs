d9106ee8676b8d3cca35be459944244d5d41c374
20wh1a1298 <85934726+20wh1a1298@users.noreply.github.com>
2021-08-28 18:35:35 +0530

    Add files via upload

---
+++ swap.cpp
@@ -0,0 +1,9 @@
+void main()
+{
+	int *p1=NULL,a1=2,a2=3,*p2=NULL;
+	p1 = &a1;
+	p2 = &a2;
+	printf("valuesof p1 and p2 are : %d %d\n",*p1,*p2);
+	printf("after swaping p1 p2 :%d %d",*p2,*p1);
+	return 1;
+}
